436535b0a0a3ca7d6eb9740d9c35361c5caa03fa
Godfrey <169153669+blakboi305@users.noreply.github.com>
2024-11-14 14:22:14 -0500

    Initial commit

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-west-1_l2NsuJZeA', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4korelfn4c8qrc96iho3p33ak1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://idc5her2fj.execute-api.us-west-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
